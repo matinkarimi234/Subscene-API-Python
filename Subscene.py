e200cfe649303a4146dd9b70bbddff443e775aa2
Matin <overwatch@pc.domain.name>
2020-05-12 14:32:11 -0400

    Class and defs Added

---
+++ Subscene.py
@@ -0,0 +1,44 @@
+class Subscene:
+    def __init__(self):
+        
+        pass
+    def curl_get_contents(url):
+        
+        pass
+    def curl_post(url,params = None):
+        pass
+    def search(title,exit_on_bad_req = False):
+
+        pass
+
+    def getSubtitles(url,exit_on_bad_req= False):
+
+        pass
+
+    def getSubtitleInfo(url,exit_on_bad_req = False):
+
+        pass
+
+    def getHome():
+
+        pass
+
+    def getDownload(url,filename):
+
+        pass
+
+    def setLanguages(languages = []):
+
+        pass
+
+    def login(username,password):
+    
+        pass
+
+    def xpathQuery(query,html):
+
+        pass
+
+    def isLoggedIn(html):
+
+        pass
